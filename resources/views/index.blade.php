80d1a2a84ccc3e458faaabc740160518e984f72a
RodrigoMejiaDiaz <rodrigoemdiaz@hotmail.com>
2019-06-12 15:48:18 -0500

    Cambio de color y barra de navegación

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 
-
 <div class="container">
 		<div class="card-columns">
 			@foreach($articulos as $articulo)
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,45 +19,56 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body style="background-color: #e3f2fd;">
+<body class="bg-primary">
     <div id="app">
 
-
-        <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
+        <nav class="navbar sticky-top navbar-expand-lg navbar-light " style="background-color: #e3f2fd;">
           <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="" alt="Logo" class="rounded float-left">
+            <img src="https://lh3.googleusercontent.com/4um9ZFTEq_zdLkiCJrqACIcDJ0MrXpmCiBbRugFP-Fg1lBg2lHF-pCKvYeYodAQCdDp4rgxuiqMObrDaa18lYmBVTvVq0Hs3haLTvQjxpVn61uHltU0N-YFaqWOZCPPGhejvolLlij3scfhADxzkTi8tK71qkAHnHalM4QtPjqsH8_DNJ5BUoCBlYCbEQa3Js1uf4iVKelssAEorjqtS6yUgvuzU7C4ngiec2QNUrnAslSqV4cZ6jlxH5hf5HvZ_2tteRO-ckw4liaVubUvQ7nbmyinLZgLXitJdJ7was_6V0Fzg9yT4qE7JebLqJukuMd-c2KiUTHTkt-MYt-RCzxV2JSivXYqZJ1iXdyoj3WyM_Auv3xnQuuTYWEu6Uu-mZe38R2GQZodfRNuBCA2e8BfvzQqLpfFgWiisiFDg8cq0_U8otYAbop9zgdOZPfUzLFSuvnjEPb_ehEaVbVe6LAK0uBzFi6YjVHxG8lY0pWnpGCnmTJJ1LzAHPdD8eK20dzBILpkZ90W_qz4sr3jfR72-a4Fwra2Pixixn4ikE_vkJj-Dkl0IBMKno9rG4r7D27S59sdmG1FtZsGTQDL7koSKNeZTf7hVCIe1h3CAwuAYeFlP6FJSq0ILJoxb8lxmXpV99vY4aCp8iHWx47C_1O49BmLeIg=w1038-h657-no" width="100" height="70" class="d-inline-block align-top" alt="Logo">
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="#">Action</a>
-                  <a class="dropdown-item" href="#">Another action</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-              </li>
+          <div class="collapse navbar-collapse offcanvas-collapse" id="navbarSupportedContent">
+
+            <ul class="navbar-nav ml-auto ">
+
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                            <a class="dropdown-item" href="">Editar Usuario</a>
+                            
+                        </div>
+                    </li>
+                @endguest
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
           </div>
         </nav>
 
