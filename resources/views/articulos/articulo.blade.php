3bf9f9189145c97bdf250f290108633069ac3f71
Rodrigo Mejia Diaz <rodrigoemdiaz@hotmail.com>
2019-06-14 01:09:48 -0500

    Vistas app y artículos modificados

--- resources/views/articulos/articulo.blade.php
+++ resources/views/articulos/articulo.blade.php
@@ -4,34 +4,61 @@
 
 <div class="container">
 
-	<div class="float-left ">
-		<div class="img-field">
-			<img src="{{ asset('images/' . $articulo->img )}}" class="img-thumbnail" alt="Imagen del articulo">
+
+	<div class="context-header">
+		<div class="grid-container">
+			<div class="item-header">
+				<div class="item-header__top">
+					<div class="item-header__title">
+						<h1 class="t-heading">{{ $articulo->nom_art }}</h1>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
-
-	<div class="float-right">
-		<h4>S/.{{ $articulo->precio }}</h4>
-		<br>
-		<h4>Stock: <small>{{ $articulo->stock }}</small> </h4>
-		<br>
-		<h5>{{ $articulo->nom_cat }}</h5>
-		<br>
-		<button class="btn btn-success btn-lg">Comprar</button>
-	</div>
-
-	<div class="mx-auto" style="width: 200px;">
-		<h1 class="display-4">{{ $articulo->nom_art }}</h1>
-
-		
-		<h5>{{ $articulo->des_art }}</h5>
-		<br>
-		
 		
 	</div>
 
 
+	<div class="content-main" id="content">
+		<div class="grid-container">
+			<div class="content-s">
+				<div class="box--no-padding">
+					<div class="item-preview -preview-live">
+						<a href="">
+							<img alt="{{ $articulo->nom_art }}" src="{{ asset('images/' . $articulo->img )}}" sizes="(min-width: 1024px) 590px, (min-width: 1px) 100vw, 600px">
+						</a>
+					</div>
+				</div>
+				<div data-view="toggleItemDescription">
+					<div class="js-item-description item-description has-toggle">
+						{{ $articulo->des_art }}
+					</div>
+				</div>
+			</div>
 
+			
+		</div>
+
+		
+	</div>
+
+	<div class="sidebar sidebar-right">
+				<div class="pricebox-container">
+					<div class="purchase-panel">
+						<div id="purchase-form" class="purchase-form">
+							<form data-view="purchaseForm">
+								<div>
+									<div data-view="itemVariantSelector">
+										{{ $articulo->precio }}
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+
+	
 
 </div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,59 +21,59 @@
 </head>
 <body style="background-color: #e3f2fd;">
     <div id="app">
-        <nav class="navbar sticky-top navbar-expand-md navbar-dark bg-primary">
-            <div class="container">
-                <div class="navbar-left">
-                    <a class="navbar-logo" href="{{ url('/') }}">
-                    <img src="https://lh3.googleusercontent.com/05_Yu84LVg8uTC2eWXLcxCOwSP3TZM90Gksf_aPPer93NpWOMjU9tKOLCwpje2v6I0sk07cTCSNQ32xqBCD7WFo1GwV8znvf7BldF2pH78RAaZXhEYo0lG0FodpyelmXFsEyewK8E8HzQykcjzISaKYBHhpTd2GOkuXDWSLRQLOFoxfanQyZkdRxordvI7h7rCqJVXDKS2RROx0Q7f6bI2TMdBE64h0irmQO6PMBLgoSfoy5Gcr7Ciu8wL2mUrrNbbvfCnTiq5OyjWK1q5S-JU5qdSmd5jQ6Oo7-MqGLHT3K-t6og5Z_zdS8J42DRzu2lY4fmvpiewtmlH4YCkf-TBech4sqqGdONLR7fT9xbSep4GQot2mZZfNTbPoYzw7zR-T4od7xlLhSdokCpUukO4J4gZCS-cVsmS9GqfVdaCVazNDfq1nfQ1p-gTYFyy2TYHOrvDwEvL8sXvIK_6B73f9xKLu-mJnsvgNu6lWhsq4l106NrSry2mXKNrkcSsbSeHZPWl5MbNLnI5BFEXwKE6NRqzuC9YhmVNuIAxm2xDdbGaKWtqSNrvHbwGSEI2AbuN9kRl9JZQGIJNjFZSsMUEyr2b3zEy7Yxb6vu2wBBri0Nqetsmmq5fde_uCxCa59S2oZ2A26KMizO1E1yCmIkrw_okz9ig=w1246-h789-no" alt="Logo" class="rounded float-left" style="float:left;width:10%;height:12%;">
-                </a>
-                </div>
-                
-                
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-primary">
+                 <a class="navbar-brand" href="{{ url('/') }}">
+                   <img src="https://lh3.googleusercontent.com/xFaBKQQ8VJYWNtYvamzxHPChZ5r1JtfDbpCxbLfxAu6ahOpF65aIncQaCfx57WOWyZ9aDCngiQpveTzE2qNvy91IKWnONO4QNMlN3JBeh9EL37-lgmGjZXUdkmrI1HTwSafGs2sh9jGs71tp_psr71flL0jiSVOmAHku0vCrzejRDlCSGXLAWjng5Kd3olJU78yF78KtyYAK4caz3huMlgIMMphES_4ZOSFLnmtxIKcDnf5-Z7TAQ06anfLSBNzbID7XuRXUnB3g5DEprvFh85FNWtLx5NNhVJ6PhsGJsrkNA_1olTl8sTWx_a7dbZ2gHhMDH6cokBbYblZQom35cRtAUsnVifbm7u4dVvXHOCaUwZ-fpyktEPfLDccqqif2x7PzALPyCpJRzAj8WTe-a-vuDyyWkEn0AuQFEAwgBHuMWXn5Ia2gFdzagETbYNfU7hYzBnPztb4saQxKPag2u19FDMe1nKGLN6RduxSfGuS8l7jOUNohplNEIbdoQD4Je9y7t-rlnkSOaA1MCHCYDYOFs7YmrABG3oJLQS5CqQcCNPvku9LCb3cD-iTI9Wb8gvDfxLp9QI_hQQVemLMToUj7zGKLYUJu_A0=s1038-w1038-h657-no" width="100" height="70" class="d-inline-block align-top" alt="Logo">
+                 </a>
+                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                   <span class="navbar-toggler-icon"></span>
+                 </button>
 
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
+                 <div class="collapse navbar-collapse offcanvas-collapse" id="navbarSupportedContent">
+                    
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
-
+                        <form class="form-inline">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                                    </form>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                               
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
                                 </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                   <a class="dropdown-item" href="{{ route('logout') }}"
+                                      onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                       {{ __('Logout') }}
+                                   </a>
+                                   <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                       @csrf
+                                   </form>
+                                   <a class="dropdown-item" href="">Editar Usuario</a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                               </div>
+                           </li>
+                       @endguest
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+
+                        
                     </ul>
                 </div>
 
