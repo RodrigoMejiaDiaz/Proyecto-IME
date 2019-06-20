b590771f793f17569dc15adc06126cfeee64b9bc
RodrigoMejiaDiaz <rodrigoemdiaz@hotmail.com>
2019-06-20 13:57:02 -0500

    Correción user edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,7 +38,7 @@ class UserController extends Controller
 
     public function edit(User $user)
     {
-        return view('auth.edit');
+        return view('user.edit');
     }
 
     public function update(Request $request,$id)
