85382715020a262dd4bdfaaebccc96882f942ea7
RodrigoMejiaDiaz <rodrigoemdiaz@hotmail.com>
2019-06-20 13:58:11 -0500

    Correción 2 user edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Compra;
 use App\Articulo;
 use App\Categoria_Articulo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 
