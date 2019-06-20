bc7aa1966287fd087c06ab875cf2fc0392f75e68
RodrigoMejiaDiaz <rodrigoemdiaz@hotmail.com>
2019-06-20 16:48:04 -0500

    Correción error navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,25 +98,6 @@
         </nav>
 
       </div>
-      <nav class="nav nav-tabs p-4">
-        <li class="nav-item">
-            <a class="flex-sm-fill text-sm-center nav-link active" href="{{ route('home') }}">Todos</a>
-        </li> 
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Categorías</a>
-            
-            <div class="dropdown-menu">
-                @foreach($categorias_articulos as $categoria_articulo)
-                <a class="dropdown-item" href="{{ action('ArticuloController@categorias_articulos', $categoria_articulo->nom_cat) }}">{{$categoria_articulo->nom_cat}}</a>
-                 @endforeach
-            </div>
-
-           
-            
-        </li>
-       
-       
-      </nav>
 
 
         <main class="py-4 bg-light text-dark" >
